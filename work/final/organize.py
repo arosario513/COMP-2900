3c7f2c1266698ca88a2298dae764aa8c3596ad3a
arosario513 <a.rosario51304@gmail.com>
2024-11-23 13:38:00 -0400

    made it graph the csv

--- work/final/organize.py
+++ work/final/organize.py
@@ -1,14 +1,37 @@
 #!venv/bin/python3
-'''Organizes the results and saves them as CSVs'''
+'''Organizes the results and graphs them'''
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 sdf: pd.DataFrame = pd.read_csv("./selection_results.csv") \
+    .set_index("Input Size") \
     .pivot_table(index=["Input Size"], columns=["Case"], values=["Time"])
 
 bdf: pd.DataFrame = pd.read_csv("./bubble_results.csv") \
+    .set_index("Input Size") \
     .pivot_table(index=["Input Size"], columns=["Case"], values=["Time"])
 
 
-sdf.to_csv("selection_table.csv")
-bdf.to_csv("bubble_table.csv")
+plt.style.use("seaborn-v0_8")
+
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8), sharex=True, sharey=True)
+for i, j in zip(sdf.columns, ["green", "red", "blue"]):
+    ax1.plot(sdf.index, sdf[i], color=j)
+
+for i, j in zip(bdf.columns, ["green", "red", "blue"]):
+    ax2.plot(bdf.index, bdf[i], color=j)
+
+
+ax1.set_title("Selection Sort")
+ax2.set_title("Bubble Sort")
+
+fig.suptitle("Algorithm Analysis", fontsize=20)
+fig.supxlabel("Input Size")
+fig.supylabel("Time (Seconds)")
+fig.tight_layout()
+
+plt.xlim(10_000, 100_000)
+plt.xticks(rotation=45)
+plt.legend(["Best", "Worst", "Random"])
+plt.show()
